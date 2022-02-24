ddadf14ad7b042159fd4fd65b0e909b59e3e3690
Lucas Souza <lucas_nevestj@hotmail.com>
2022-02-24 14:51:01 -0300

    comentários sobre keys úncicas

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -7,13 +7,19 @@ class ListaDeNotas extends Component
         return (
             <div className="container">
                 <ul className="list-group">
+                {/*
+                 usando um array map do js para criar um loop de li's. 
+                 o atributo 'key' na tag 'li' permite ao react identificar cada item renderizado,
+                 por isso deve ser único. para garantir isso, fiz uma concatenação para atribuir um
+                 nome individual para cada 'key' 
+                */}
                 {Array.of("Trabalho", "Estudos", "Geral").map((categoria) => {
                     return (
-                        <li className="list-group-item d-flex justify-content-between align-items-start">
-                            <div class="ms-2 me-auto">
+                        <li key={"id" + categoria} className="list-group-item d-flex justify-content-between align-items-start">
+                            <div className="ms-2 me-auto">
                                 <CardNota />
                             </div>
-                            <span class="badge bg-primary rounded-pill">{categoria}</span>
+                            <span className="badge bg-primary rounded-pill">{categoria}</span>
                         </li>
                     );
                 })}

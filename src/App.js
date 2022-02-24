c89e205285ecbb2b85647e49351ea61dec1ef07a
Lucas Souza <lucas_nevestj@hotmail.com>
2022-02-24 13:29:59 -0300

    criando primeira class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import React from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+
 function App() {
   return (
-    <h1>App com React</h1>
+    <div className="container d-flex flex-row">
+    <div className="container mb-3">
+      <form>
+        <div className="mb-3">
+          <label className="form-label" for="inputTitulo">Título da Nota</label>
+          <input className="form-control" type="text" id="inputTitulo"></input>
+        </div>
+        <div className="mb-3">
+          <label className="form-label" for="textoNota">Texto da Nota</label>
+          <textarea className="form-control" id="textoNota" placeholder="Escreva sua nota..."></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">Salvar</button>
+      </form>
+    </div>
+    <ListaDeNotas/>
+    </div>
   );
 }
 

240b3e41c1aaf8a2fd0fb694ed2dfaf8d339e3a3
Lucas Souza <lucas_nevestj@hotmail.com>
2022-02-25 15:37:14 -0300

    coments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormDeNotas from "./components/FormDeNotas";
 
+/*
+State: Serve para guardar valores/estado que podem vir a mudar com a interação do usuário com o componente, tendo efeito na renderização do mesmo, o state pode ser passado como props!
+
+Props: Valores para a configuração de um componente, essas props são passadas pelo elemento acima (que utiliza o componente que irá receber) e são imutáveis, utilizado para a comunicação de componentes.
+*/
+
 class App extends Component {
   constructor() {
     super();
